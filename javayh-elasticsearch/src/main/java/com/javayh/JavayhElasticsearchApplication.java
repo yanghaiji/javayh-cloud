c6e739d7b0e934153517196b2e054e48fba5d741
杨海吉 <37392164+Dylan-haiji@users.noreply.github.com>
2019-06-25 17:48:40 +0800

    Update JavayhElasticsearchApplication.java

--- javayh-elasticsearch/src/main/java/com/javayh/JavayhElasticsearchApplication.java
+++ javayh-elasticsearch/src/main/java/com/javayh/JavayhElasticsearchApplication.java
@@ -12,11 +12,4 @@ public class JavayhElasticsearchApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavayhElasticsearchApplication.class, args);
     }
-
-    /**
-     * 朝阳门 内大街 8hao 朝阳首府 211
-     * 赵嘉欣
-     * 18513066039
-     */
-
 }
