7acd66683a935f93f6212a25f2c4d6ec51c99e2b
Dylan-haiji <yanghaij327@163.com>
2019-07-06 14:45:50 +0800

    添加加密方法

---
+++ javayh-commons/src/main/java/com/javayh/encryption/AESUtil.java
@@ -0,0 +1,117 @@
+package com.javayh.encryption;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ * @author Dylan Yang
+ * @Description: AESUtil
+ * @Title: AESUtil
+ * @ProjectName javayh-oauth2
+ * @date 2019/7/5 15:49
+ */
+public class AESUtil {
+
+    private static final String KEY_ALGORITHM = "AES";
+    private static final String DEFAULT_CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";//默认的加密算法
+
+    /**
+     * AES 加密操作
+     *
+     * @param content 待加密内容
+     * @param key 加密密钥
+     * @return 返回Base64转码后的加密数据
+     */
+    public static String encrypt(String content, String key) {
+        try {
+            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);// 创建密码器
+            byte[] byteContent = content.getBytes("utf-8");
+            //初始化为加密模式的密码器
+            //方法一
+            //cipher.init(Cipher.DECRYPT_MODE, getSecretKey(key));
+            //方法二
+            cipher.init(Cipher.ENCRYPT_MODE, getSecretKeySpec(key));
+            byte[] result = cipher.doFinal(byteContent);// 加密
+            return Base64.encodeBase64String(result);//通过Base64转码返回
+        } catch (Exception ex) {
+            Logger.getLogger(AESUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    /**
+     * AES 解密操作
+     *
+     * @param content
+     * @param key
+     * @return
+     */
+    public static String decrypt(String content, String key) {
+        try {
+            //实例化
+            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+            // 使用密钥初始化，设置为解密模式
+            //方法一
+            //cipher.init(Cipher.DECRYPT_MODE, getSecretKey(key));
+            //方法二
+            cipher.init(Cipher.DECRYPT_MODE, getSecretKeySpec(key));
+            //执行操作
+            byte[] result = cipher.doFinal(Base64.decodeBase64(content));
+            return new String(result, "utf-8");
+        } catch (Exception ex) {
+            Logger.getLogger(AESUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    /**
+     * 生成加密秘钥
+     *
+     * @return
+     */
+    private static SecretKey getSecretKey(final String key) {
+        try {
+            SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), KEY_ALGORITHM);
+            SecretKey sk = SecretKeyFactory.getInstance(KEY_ALGORITHM).generateSecret(keySpec);
+            return sk;
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 生成加密秘钥
+     *
+     * @return
+     */
+    private static SecretKeySpec getSecretKeySpec(final String key) {
+        //返回生成指定算法密钥生成器的 KeyGenerator 对象
+        KeyGenerator kg = null;
+        try {
+            kg = KeyGenerator.getInstance(KEY_ALGORITHM);
+            //AES 要求密钥长度为 128
+            kg.init(128, new SecureRandom(key.getBytes()));
+            //生成一个密钥
+            SecretKey secretKey = kg.generateKey();
+            return new SecretKeySpec(secretKey.getEncoded(), KEY_ALGORITHM);// 转换为AES专用密钥
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(AESUtil.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+
+}
+
